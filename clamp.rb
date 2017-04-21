63c8eabe16d85041341d5b86370ac6cbb0ae71f3
Rezart Qelibari <rqelibari@users.noreply.github.com>
2017-04-21 02:28:39 +0200

    Expand abbrev. revision as brew does not accept it.

--- clamp.rb
+++ clamp.rb
@@ -1,7 +1,7 @@
 class Clamp < Formula
   desc "command-line tool for local MAMP management"
   homepage "https://jide.github.io/clamp"
-  url "https://github.com/jide/clamp.git", :using => :git, :tag => "1.4.1", :revision => "e00bfa9"
+  url "https://github.com/jide/clamp.git", :using => :git, :tag => "1.4.1", :revision => "e00bfa908c2b15840600e1f26d08807f4cee0998"
 
   depends_on "mariadb"
 
