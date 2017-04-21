bdbe6ad3ee16a34d36dfb6ae485e1fb36211760e
Rezart Qelibari <rqelibari@users.noreply.github.com>
2017-04-21 02:25:41 +0200

    Fix audit errors in forumla. Update ver. to latest.

--- clamp.rb
+++ clamp.rb
@@ -1,17 +1,14 @@
-require "formula"
-
 class Clamp < Formula
-  homepage "http://jide.github.io/clamp"
-  url "https://github.com/jide/clamp.git", :using => :git, :tag => "1.3.3"
-  version "1.3.3"
-  sha256 "de21372944245b40db2b6feed07b88d993a11b08be9262e6ba5d2f354097c5ec"
+  desc "command-line tool for local MAMP management"
+  homepage "https://jide.github.io/clamp"
+  url "https://github.com/jide/clamp.git", :using => :git, :tag => "1.4.1", :revision => "e00bfa9"
 
   depends_on "mariadb"
 
   def install
-    inreplace "clamp", /\/usr\/local\/clamp/, prefix
+    inreplace "clamp", %r{/usr/local/clamp}, prefix
     prefix.install Dir["*"]
-    bin.install_symlink '../clamp'
+    bin.install_symlink "../clamp"
   end
 
   test do
